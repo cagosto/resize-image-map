3d5c789bb8493e595faffda72e556eca53c185f5
Carlos Agosto <carlos.agosto@gmail.com>
2018-07-03 23:38:05 -0400

    fix rework loops.

--- image-resize.js
+++ image-resize.js
@@ -6,8 +6,8 @@ export default class ImageResize {
     this.imageH = height
     this.imageMap = document.querySelector(element)
     const mapId = this.imageMap.getAttribute('usemap')
-    this.area = document.querySelector(`map[name="${mapId.substring(1, mapId.length)}"]`).children
-
+    const area = document.querySelector(`map[name="${mapId.substring(1, mapId.length)}"]`).children
+    this.areaArray = Array.from(area)
     window.addEventListener('resize', this.resizeEvent)
 
     setTimeout(this.imgMap, 500)
@@ -17,17 +17,16 @@ export default class ImageResize {
     const wPercent = this.imageMap.offsetWidth / 100
     const hPercent = this.imageMap.offsetHeight / 100
 
-    for (let i = 0; i < this.area.length; i++) {
-      const area = this.area[i]
-      const coords = this.getCoords(area).split(",")
+    this.areaArray.forEach( elem => {
+      const coords = this.getCoords(elem).split(",")
       const coordsPercent = coords.map( (coord, index) => {
         return (index % 2) === 0
           ? parseInt(((coords[index] / imageW) * 100) * wPercent, 10)
           : parseInt(((coords[index] / imageH) * 100) * hPercent, 10)
       }).join()
 
-      area.setAttribute('coords', coordsPercent)
-    }
+      elem.setAttribute('coords', coordsPercent)
+    })
   }
   getCoords = (elem) => {
     let areaCords = elem.dataset.coords
