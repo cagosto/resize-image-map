381128a97b3f498403e8b4d841392caeae41705f
Carlos Agosto <carlos.agosto@gmail.com>
2018-07-08 11:47:39 -0400

    chore update script notes.

--- image-resize.js
+++ image-resize.js
@@ -1,8 +1,20 @@
+/**
+ * Example config
+ * {
+    width: 1024, Natural with
+    height: 768, Natural with
+    element : '#power-puff__map' Selector
+   }
+ */
 export default class ImageResize {
+  /**
+   * constructor - make image maps responsive
+   * @param {Object} config - setting for responsive image map
+   */
   constructor(config) {
     const {width, height, element} = config
 
-    this.imageW = width;
+    this.imageW = width
     this.imageH = height
     this.imageMap = document.querySelector(element)
     const mapId = this.imageMap.getAttribute('usemap')
@@ -12,6 +24,11 @@ export default class ImageResize {
     window.addEventListener('resize', this.resizeEvent)
     setTimeout(this.imgMap, 500)
   }
+  /**
+   * getCoords - get image map coordinates
+   * @param  {Node} elem - area tag
+   * @return {String} - area map coordinates
+   */
   getCoords = (elem) => {
     let areaCords = elem.dataset.coords
 
@@ -29,12 +46,22 @@ export default class ImageResize {
 
     this.areaArray.forEach(this.areaLoop)
   }
+  /**
+   * areaLoop - Loop through area tags for image map
+   * @param  {Node} area - Area tag
+   */
   areaLoop = (area) => {
     const coords = this.getCoords(area).split(",")
     const coordsPercent = coords.map(this.mapCoords).join()
 
     area.setAttribute('coords', coordsPercent)
   }
+  /**
+   * mapCoords - Set new image map coordinates based on new image width and height
+   * @param  {Num} coord - coordinates from image map array
+   * @param  {Num} index - Loop index
+   * @return {Num} - New imae map coordinates
+   */
   mapCoords = (coord, index) => {
     return (index % 2) === 0
       ? parseInt(((coord / this.imageW) * 100) * this.wPercent, 10)
